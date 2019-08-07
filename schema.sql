0514e343c3e10a57fc7e9eddbcd12a6b37f69d24
Johnny Asbury <kalico@mac.com>
2019-08-06 19:34:44 -0700

    Cleanup the DB schema

--- schema.sql
+++ schema.sql
@@ -3,28 +3,13 @@ CREATE TABLE circuits (
    circuitRef TEXT,
    name TEXT,
    location TEXT,
-   country FLOAT
-);
-CREATE TABLE constructorResults (
-   constructorResultsId INT PRIMARY KEY,
-   raceId INT,
-   constructorId INT,
-   points INT
+   country TEXT
 );
 CREATE TABLE constructors (
    constructorId INT PRIMARY KEY,
    constructorRef TEXT,
    name TEXT,
    nationality TEXT
-);
-CREATE TABLE constructorStandings (
-   constructorStandingsId INT PRIMARY KEY,
-   raceId INT,
-   constructorId INT,
-   points INT,
-   position INT,
-   positionText INT,
-   wins INT
 );
 CREATE TABLE drivers (
    driverId INT PRIMARY KEY,
@@ -35,25 +20,13 @@ CREATE TABLE drivers (
    dob DATE,
    nationality TEXT
 );
-CREATE TABLE driverStandings (
-   driverStandingsId INT PRIMARY KEY,
-   raceId INT,
-   driverId INT,
-   points INT,
-   position INT,
-   positionText INT,
-   wins INT
-);
 CREATE TABLE qualifying (
    qualifyId INT PRIMARY KEY,
    raceId INT,
    driverId INT,
    constructorId INT,
    number INT,
-   position INT,
-   q1 INTERVAL,
-   q2 INTERVAL,
-   q3 INTERVAL
+   position INT
 );
 CREATE TABLE races (
    raceId INT PRIMARY KEY,
@@ -61,6 +34,18 @@ CREATE TABLE races (
    round INT,
    circuitId INT,
    name TEXT,
-   date DATE,
-   time TIME
+   date DATE
+);
+CREATE TABLE results (
+   resultId INT PRIMARY KEY,
+   raceId INT,
+   driverId INT,
+   constructorId INT,
+   grid INT,
+   positionOrder INT,
+   statusId INT
+);
+CREATE TABLE status (
+   statusId INT PRIMARY KEY,
+   status TEXT
 );
