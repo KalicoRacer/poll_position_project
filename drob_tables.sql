8289883ac8c4cc30edf8bc9f3834714b11ca181a
Johnny Asbury <kalico@mac.com>
2019-08-08 19:25:46 -0700

    SQL file to drop tables from formula1data DB

---
+++ drob_tables.sql
@@ -0,0 +1,7 @@
+DROP TABLE public.circuits;
+DROP TABLE public.constructors;
+DROP TABLE public.drivers;
+DROP TABLE public.qualifying;
+DROP TABLE public.races;
+DROP TABLE public.results;
+DROP TABLE public.status;
